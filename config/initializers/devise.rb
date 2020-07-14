d2f58535c8ec342febc49728bbe067d6aa172e80
Bianjie <bianjieandara@gmail.com>
2020-07-14 15:58:07 -0300

    devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,4 +1,5 @@
 Devise.setup do |config|
   #config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
   config.navigational_formats = [:json]
+  config.secret_key = 'b106452a7512a2a53c876b7104acc04f8044abc1b07cddad0dd132724ed2526a3082587aaa182b48d23e16096cfb278b7d99e733cb625dbd52f4fea14928bb27'
 end
